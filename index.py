6cc7333fed432f8367f6e81ce5202cf41fd11d6e
Fadi Ramzi Mohammed <20383171+fadiramzi@users.noreply.github.com>
2022-08-07 17:41:17 +0300

    reading from staging.csv in staging env

--- index.py
+++ index.py
@@ -1,7 +1,7 @@
 import csv
 #  open the file
 try:
-    file = open('dev.csv','r')
+    file = open('staging.csv','r')
     rowsReader = csv.reader(file)
 # ['#', 'name', 'age', 'email']
 # [1, dd, fsdf, df]
